6461ca1463dca52ea29b7498f1d85332ae701125
Kunal Gupta <k00unal@gmail.com>
2019-07-05 16:04:21 +0530

    BEM Classes and css updated

--- src/components/simple-slider/simple-slider.tsx
+++ src/components/simple-slider/simple-slider.tsx
@@ -12,25 +12,25 @@ export class SimpleSlider {
   @Prop() fifthslide: string = "5";
 
   componentDidLoad() {
-    let slides = document.querySelectorAll("#slides .slide");
+    let slides = document.querySelectorAll(".slides .slides__slide");
     let currentSlide = 0;
     setInterval(nextSlide, 6000);
 
     function nextSlide() {
-      slides[currentSlide].className = "slide";
+      slides[currentSlide].className = "slides__slide";
       currentSlide = (currentSlide + 1) % slides.length;
-      slides[currentSlide].className = "slide showing";
+      slides[currentSlide].className = "slides__slide showing";
     }
   }
 
   render() {
     return (
-      <ul id="slides">
-        <li class="slide showing">{this.firstslide}</li>
-        <li class="slide">{this.secondslide}</li>
-        <li class="slide">{this.thirdslide}</li>
-        <li class="slide">{this.fourthslide}</li>
-        <li class="slide">{this.fifthslide}</li>
+      <ul class="slides">
+        <li class="slides__slide showing">{this.firstslide}</li>
+        <li class="slides__slide">{this.secondslide}</li>
+        <li class="slides__slide">{this.thirdslide}</li>
+        <li class="slides__slide">{this.fourthslide}</li>
+        <li class="slides__slide">{this.fifthslide}</li>
       </ul>
     );
   }
